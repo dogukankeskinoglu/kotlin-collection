c7391fc3d19fc2d5602e3fa3ccdb4db33a0fe256
Doğukan Keskinoğlu <33787244+dogukankeskinoglu@users.noreply.github.com>
2021-06-18 11:38:00 +0300

    Update map.kt

--- CollectionFunction/map.kt
+++ CollectionFunction/map.kt
@@ -5,4 +5,5 @@ fun main(){
     val numbers=(0..10).toMutableList()
 
     print(numbers.map { if (it%2==0) "$it:even" else "$it:odd" })
+    //[0:even, 1:odd, 2:even, 3:odd, 4:even, 5:odd, 6:even, 7:odd, 8:even, 9:odd, 10:even]
 }
