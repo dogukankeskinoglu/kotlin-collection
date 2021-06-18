9de5548b5aef80dfe4f526b4f94028cff4cee5f3
Doğukan Keskinoğlu <33787244+dogukankeskinoglu@users.noreply.github.com>
2021-06-18 11:14:51 +0300

    Update Copy.kt

--- CollectionFunction/Copy.kt
+++ CollectionFunction/Copy.kt
@@ -8,6 +8,6 @@ fun main(){
     nameList.add("Hasan")
     println("Main List:$nameList \nCopy List:$names")
 
-    //Main List:[Dogukan, Ali, Hamza, Ceren, Selin, Cansu, Irem, Fatih, Yağız]
-    //Copy List:[Dogukan, Ali, Hamza, Ceren, Selin, Cansu, Irem, Fatih, Yağız, Hasan]
+    //Main List:[Dogukan, Ali, Hamza, Ceren, Selin, Cansu, Irem, Fatih, Yağız, Hasan]
+    //Copy List:[Dogukan, Ali, Hamza, Ceren, Selin, Cansu, Irem, Fatih, Yağız]
 }
