fe09f49825143701eb04ac8a1809467d8dc295eb
Doğukan Keskinoğlu <33787244+dogukankeskinoglu@users.noreply.github.com>
2021-06-18 11:13:51 +0300

    Update Copy.kt

--- CollectionFunction/Copy.kt
+++ CollectionFunction/Copy.kt
@@ -1,11 +1,11 @@
 package CollectionFunction
 
 fun main(){
-    val nameList= listOf<String>("Dogukan","Ali","Hamza","Ceren","Selin","Cansu","Irem","Fatih","Yağız")
+    val nameList= mutableListOf<String>("Dogukan","Ali","Hamza","Ceren","Selin","Cansu","Irem","Fatih","Yağız")
 
     val names=nameList.toMutableList()
     val names2=nameList.toList()//Read-Only
-    names.add("Hasan")
+    nameList.add("Hasan")
     println("Main List:$nameList \nCopy List:$names")
 
     //Main List:[Dogukan, Ali, Hamza, Ceren, Selin, Cansu, Irem, Fatih, Yağız]
